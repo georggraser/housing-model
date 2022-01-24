f0ad538551baf1a19317817680c0608b0a1f35f9
Georg Graser <georg.graser@uni-flensburg.de>
2022-01-24 14:17:59 +0100

    whitespaces

--- model.py
+++ model.py
@@ -13,17 +13,17 @@ POPULATION = 'data/population.csv'
 
 
 def main():
-	# load all relevant input data from the data loader
-	dl = data_loader.DataLoader()
-	main_parameter_df = dl.load_csv(MAIN_PARAMETER)
-	distribution_buildings_df = dl.load_csv(DISTRIBUTION_BUILDINGS)
-	population_df = dl.load_csv(POPULATION)
-	
-	print(distribution_buildings_df)
+  # load all relevant input data from the data loader
+  dl = data_loader.DataLoader()
+  main_parameter_df = dl.load_csv(MAIN_PARAMETER)
+  distribution_buildings_df = dl.load_csv(DISTRIBUTION_BUILDINGS)
+  population_df = dl.load_csv(POPULATION)
+  
+  print(distribution_buildings_df)
 
 
 if __name__ == '__main__':
-	'''
-	starts the main function when the file is being called
-	'''
-	main()
+  '''
+  starts the main function when the file is being called
+  '''
+  main()
