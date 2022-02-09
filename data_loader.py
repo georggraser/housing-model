ec912cbf42aad25b61d93ee8bfb4d7bfef60d823
Georg Graser <georg.graser@uni-flensburg.de>
2022-02-09 10:00:57 +0100

    applied strict styleguide rules

--- data_loader.py
+++ data_loader.py
@@ -15,14 +15,16 @@ class DataLoader():
     def load_share_buildings(self, path_share_buildings):
         df = pd.read_excel(path_share_buildings)
         total_living_space = df['living_space_mio.m2'].sum()
-        relative_living_space = [x / total_living_space for x in df['living_space_mio.m2'] if x is not nan]
+        relative_living_space = [x / total_living_space
+                                 for x in df['living_space_mio.m2']
+                                 if x is not nan]
         df.loc[:, 'percent_living_space'] = relative_living_space
-        #print(df.to_markdown())
+        # print(df.to_markdown())
         return df
 
     def load_demographic_developement(self, path_demographic_dev):
-        # TODO: check if the parameters should be changeable in a spreadsheet somewhere
-        # and being called from there
+        # TODO: check if the parameters should be changeable in a spreadsheet
+        # somewhere and being called from there
 
         # parameters for calling the demographic developement
         columns = 'A, C:AR'
@@ -35,49 +37,48 @@ class DataLoader():
             path_demographic_dev, usecols=columns, names=names, header=None,
             skiprows=rows_start, nrows=rows_end - rows_start)
 
-        #print(df)
+        # print(df)
         return df
 
-
     def load_tabula(self, path_tabula):
-        # TODO: check if the parameters should be changeable in a spreadsheet somewhere
-        # and being called from there
+        # TODO: check if the parameters should be changeable in a spreadsheet
+        # somewhere and being called from there
 
         # parameters for calling the tabula
         sheet_name = 'DE Tables & Charts'
-        columns_used = {'identifier' : 'F',
-                        'building_type' : 'BB',
-                        'building_code' : 'BC', 
-                        'energy_reference_area' : 'BH',
-                        'heat_provided' : 'BN',
-                        'building_variant' : 'BF',
-                        'hot_water_provided' : 'BO',
-                        'space_heat_need' : 'BL',
-                        'hot_water_need' : 'BM'}
+        columns_used = {'identifier': 'F',
+                        'building_type': 'BB',
+                        'building_code': 'BC',
+                        'energy_reference_area': 'BH',
+                        'heat_provided': 'BN',
+                        'building_variant': 'BF',
+                        'hot_water_provided': 'BO',
+                        'space_heat_need': 'BL',
+                        'hot_water_need': 'BM'}
         columns = ''
         names = []
         for key, value in columns_used.items():
             names.append(key)
-            if columns is "":
+            if columns == "":
                 columns = value
-            else: 
+            else:
                 columns += ", " + value
 
         # load the tabula with a little method, params are the start
         # and end line numbers
-        def loader(x,y): 
+        def loader(x, y):
             return pd.read_excel(
-                path_tabula, sheet_name=sheet_name, usecols=columns, 
+                path_tabula, sheet_name=sheet_name, usecols=columns,
                 skiprows=x-1, nrows=y-x+1, header=None, names=names)
-            
-        rows_start_end = [[147, 278], [279, 281], [288, 290], [297, 299], 
-                            [306, 308], [315, 317],[324, 326]]
+
+        rows_start_end = [[147, 278], [279, 281], [288, 290], [297, 299],
+                          [306, 308], [315, 317], [324, 326]]
         # dataframe with rows from line 147 to 278
         df = loader(rows_start_end[0][0], rows_start_end[0][1])
         # add the rest to the dataframe
         for x, y in rows_start_end[1:]:
-            df = df.append(loader(x,y), ignore_index=True)
+            df = df.append(loader(x, y), ignore_index=True)
         # fill nans from merged cells with the correct texts
         df = df.fillna(method='ffill', axis=0)
-        #print(df.to_markdown())
+        # print(df.to_markdown())
         return df
--- model.py
+++ model.py
@@ -3,36 +3,42 @@
 @author: guuug
 """
 
-###### IMPORTS ######
+# IMPORTS
 
 import data_loader
 import os
 
-###### GLOBALS ######
+# GLOBALS
 
 # demolition and restauration rate of building classes
 # TODO: short description where the data originates or how it is calculated
-PATH_DISTRIBUTION_BUILDINGS = os.path.join('data','distribution_buildings.csv')
+PATH_DISTRIBUTION_BUILDINGS = os.path.join(
+                'data', 'distribution_buildings.csv')
 
 # demographic developement of germany based on different population models
 # source: https://www-genesis.destatis.de/genesis//online?operation=table&code=12421-0001&bypass=true&levelindex=0&levelid=1643115777925#abreadcrumb
-PATH_DEMOGRAPHIC_DEVELOPEMENT = os.path.join('data','12421-0001.xlsx')
+PATH_DEMOGRAPHIC_DEVELOPEMENT = os.path.join(
+                'data', '12421-0001.xlsx')
 
 # TODO: short description, link to table
-PATH_TABULA = os.path.join('data','TABULA-Analyses_DE-Typology_ResultData.xlsx')
+PATH_TABULA = os.path.join(
+                'data', 'TABULA-Analyses_DE-Typology_ResultData.xlsx')
 
 # TODO: add description
-PATH_SHARE_BUILDINGS_2019 = os.path.join('data','share_buildings_2019.xlsx')
+PATH_SHARE_BUILDINGS_2019 = os.path.join(
+                'data', 'share_buildings_2019.xlsx')
 
 
-###### METHODS ######
+# METHODS
 
-def load_data(): 
-    dl = data_loader.DataLoader()  
+def load_data():
 
-    #df_tabula = dl.load_tabula(PATH_TABULA) 
-    #df_dem_dev = dl.load_demographic_developement(PATH_DEMOGRAPHIC_DEVELOPEMENT)
+    dl = data_loader.DataLoader()
+
+    # df_tabula = dl.load_tabula(PATH_TABULA)
+    # df_dem_dev = dl.load_demographic_developement(PATH_DEMOGRAPHIC_DEVELOPEMENT)
     df_share_buildings = dl.load_share_buildings(PATH_SHARE_BUILDINGS_2019)
+
 
 def main():
     # load all relevant input data from the data loader
